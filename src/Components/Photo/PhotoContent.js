90f7b243f2dd327bb02c082f16d6bc61a8e23272
felipejourdan <fjourdanp@gmail.com>
2022-02-01 19:05:01 +0000

    the photo modal logic and css created

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,7 +1,32 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import PhotoCommentes from './PhotoCommentes';
+import styles from './PhotoContent.module.css';
 const PhotoContent = ({ data }) => {
-  return <div></div>;
+  const { photo, comments } = data;
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.views}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/goto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
+        </div>
+      </div>
+      <PhotoCommentes id={photo.id} comments={comments} />
+    </div>
+  );
 };
 
 export default PhotoContent;
