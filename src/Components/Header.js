73dbbf1a18318667304ebd4cfc84e2310f5931f1
felipejourdan <fjourdanp@gmail.com>
2022-02-15 00:24:59 +0000

    DONE

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
-import { ReactComponent as Dogs } from '../Assets/dogs.svg';
-import { UserContext } from '../UserContext';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { UserContext } from "../UserContext";
 
 const Header = () => {
   const { data } = React.useContext(UserContext);
   const { userLogout } = React.useContext(UserContext);
 
   function handleLogout() {
-    userLogout()
- }
-  
+    userLogout();
+  }
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
@@ -20,9 +19,9 @@ const Header = () => {
           <Dogs />
         </Link>
         {data ? (
-          
-          <div className={styles.login}>Bem-vindo {data.username}
-          <button onClick={handleLogout} >Sair</button>
+          <div className={styles.login}>
+            Bem-vindo {data.username}
+            <button onClick={handleLogout}>Sair</button>
           </div>
         ) : (
           <Link className={styles.login} to="/login">
